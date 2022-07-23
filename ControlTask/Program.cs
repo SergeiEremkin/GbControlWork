a0e20ba5fd2d46005da0a1c3d3c70c7a635924a3
SergeiEremkin <ffr@list.ru>
2022-07-24 00:58:14 +0300

    added method for create random string

--- ControlTask/Program.cs
+++ ControlTask/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string CreateRandomString(int MinStrLen, int MaxStrLen)
+{
+    Random random = new Random();
+    string symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    int SymbolsLen = random.Next(MinStrLen, MaxStrLen);
+    char[] strSymbols = new char[SymbolsLen];
+
+    for (int i = 0; i < strSymbols.Length; i++)
+    {
+        strSymbols[i] = symbols[random.Next(symbols.Length)];
+    }
+    return new String(strSymbols);
+}
+string str = CreateRandomString(2,5);
+System.Console.WriteLine(str);
